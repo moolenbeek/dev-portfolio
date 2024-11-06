4bc65d6a6466d9856959ac9a696baef295b19d4c
moolenbeek <emoolenbeek@gmail.com>
2024-11-06 08:21:59 -0500

    add lucide icon

--- src/lib/components/nav/nav.svelte
+++ src/lib/components/nav/nav.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	import { CodeXml } from 'lucide-svelte';
+</script>
+
 <nav>
 	<div class="flex h-16 justify-between">
-		<span class="my-auto ml-4 text-xl font-bold text-primary">Moolenbeek</span>
+		<span class="my-auto ml-4 text-xl font-bold text-primary">
+			<span class="flex items-center"><CodeXml class="mr-2" />Moolenbeek</span>
+		</span>
 	</div>
 </nav>
